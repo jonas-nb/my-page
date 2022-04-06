3b1a07ec53c02c6ed62ed5d801810a457ab1a6c2
Jonas Batista <jonas.nb4@gmail.com>
2022-04-06 15:23:29 -0300

    Terminado o blackboard

--- src/components/AboutPage/Differentials/ColorGenerator.jsx
+++ src/components/AboutPage/Differentials/ColorGenerator.jsx
@@ -2,22 +2,28 @@ import React from "react";
 
 const ColorGenerator = () => {
   return (
-    <div className="m-auto border w-3/4 mt-10 shadow-lg shadow-white cursor-pointer grid items-center ">
+    <div className="m-auto mb-16 lg:mb-20 border w-3/4 lg:w-2/3 xl:w-1/2 mt-10 shadow-lg shadow-white cursor-pointer grid items-center ">
       <div className="grid">
-        <div className="text-sm  hover:text-red-400">#CSS-in-JS</div>
-        <div className="text-lg justify-self-end hover:text-red-400">
+        <div className="text-sm md:text-xl lg:text-2xl  hover:text-red-400">
+          #CSS-in-JS
+        </div>
+        <div className="text-lg lg:text-xl justify-self-end hover:text-red-400">
           #Axios
         </div>
       </div>
-      <div className="text-xl justify-self-end  hover:text-red-400">
+      <div className="text-xl md:text-2xl lg:text-4xl justify-self-end  hover:text-red-400">
         #Styled-Components
       </div>
-      <div className="text-4xl justify-self-center hover:text-red-400">
+      <div className="text-4xl md:text-6xl lg:text-8xl justify-self-center hover:text-red-400">
         #Tailwind
       </div>
-      <div className="text-lg  hover:text-red-400">#Vite</div>
-      <div className="text-xl  hover:text-red-400">#React Router</div>
-      <div className="text-nd justify-self-end hover:text-red-400">
+      <div className="text-lg md:text-xl lg:text-2xl  hover:text-red-400">
+        #Vite
+      </div>
+      <div className="text-xl md:text-2xl lg:text-4xl  hover:text-red-400">
+        #React Router
+      </div>
+      <div className="text-lg md:text-xl lg:text-2xl justify-self-end hover:text-red-400">
         #Reactstrap
       </div>
     </div>
--- src/components/AboutPage/Differentials/Diferentials.jsx
+++ src/components/AboutPage/Differentials/Diferentials.jsx
@@ -3,8 +3,8 @@ import ColorGenerator from "./ColorGenerator";
 
 const Diferentials = () => {
   return (
-    <div className="bg-black h-96 mt-5 text-white flex flex-col justify-between items-center font-mono">
-      <h1 className="text-3xl mt-10">My Differentials</h1>
+    <div className="bg-black  mt-5 text-white flex flex-col justify-between items-center font-mono">
+      <h1 className="text-3xl lg:text-6xl mt-10 lg:mt-16">My Differentials</h1>
       <ColorGenerator />
     </div>
   );
