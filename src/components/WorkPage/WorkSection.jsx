e3c4299e67af1fdb47d315d6ab37e6184913948e
Jonas Batista <jonas.nb4@gmail.com>
2022-05-03 19:54:46 -0300

    continuação do projeto

--- src/components/WorkPage/WorkSection.jsx
+++ src/components/WorkPage/WorkSection.jsx
@@ -1,40 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import PageCarousel from "./PageCarousel";
-import Slider from "react-perfect-slider";
-const WorkSection = (props) => {
+import adviceImg from "./advice.png";
+import PokeinitImg from "./Pokeinit.png";
+const WorkSection = () => {
   return (
     <div>
-      {/* section carousel */}{" "}
-      <Slider
-        renderControls={(next, previous) => [
-          <button className="" onClick={previous}>
-            Previous
-          </button>,
-          <button onClick={next}>Next</button>,
-        ]}
-      >
-        <div>
-          <PageCarousel
-            title="Advice App"
-            about="Application made with React.js, it consumes an api that randomly returns advice."
-            imgEnd="https://images.pexels.com/photos/220147/pexels-photo-220147.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            code="https://github.com/jonas-nb/AdviceApp"
-            page="https://62715e70d8c4d800b5046c5d--stirring-jalebi-9adf6d.netlify.app/"
-          />
+      <section className="bg-white border mt-5">
+        <div className="border border-red-600 w-full h-48 mt-5 bg-yellow-300">
+          <h1>Pokeinit</h1>
+          <img className="" src={PokeinitImg} alt="" />
+          <ul>
+            <a href="">show code</a>
+            <a href="">show in production</a>
+          </ul>
         </div>
-        <div>
-          {" "}
-          <PageCarousel
-            title="Pokeinit"
-            about="Application made with React.js, it consumes a super fun api with the Japanese anime 'Pokemon' contains the first generation of pokemons."
-            imgEnd="https://images.pexels.com/photos/1310847/pexels-photo-1310847.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            code="https://github.com/jonas-nb/PokeInit"
-            page="https://fancy-chebakia-587538.netlify.app/"
-          />
-        </div>
-      </Slider>
-      <Routes></Routes>
+        <div className="border border-red-600 w-full h-48 mt-5 bg-white"></div>
+      </section>
     </div>
   );
 };
