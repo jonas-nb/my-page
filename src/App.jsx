19a6f24280f5df2889c5d464f1c05d0aef16603b
Jonas Batista <jonas.nb4@gmail.com>
2022-07-01 12:14:41 -0300

    inicio do update do portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import Sidebar from "./01 - header/sideBar/Sidebar";
-import { Route, Routes } from "react-router-dom";
-import AboutMe from "./02 - main/AboutMe";
-import FooterPage from "./03 - footer/FooterPage";
-import WorkSection from "./02 - main/WorkPage/WorkSection";
-import ContactPage from "./02 - main/Contact/ContactPage";
+
 const App = () => {
-  return (
-    <div>
-      <div className="">
-        <Sidebar />
-      </div>
-      <Routes>
-        <Route path="/" element={<AboutMe />} />
-        <Route path="/work" element={<WorkSection />} />
-        <Route path="/about" element={<ContactPage />} />
-      </Routes>
-      <FooterPage />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
